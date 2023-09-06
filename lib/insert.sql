18d0ee99cf13c470c41f9ece864e5363affd050b
abdhata <abdhata@outlook.com>
2023-09-06 09:50:43 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,51 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 10, "M", "brown", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Rowdy", 10, "F", "brown", "nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Tabitha ", 10, " M ", " brown ", " nice ", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (
+        " Sergeant Brown ",
+        10,
+        " M ",
+        " brown ",
+        " nice ",
+        1
+    );
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (
+        " Melissa ",
+        10,
+        " F ",
+        " brown ",
+        " nice ",
+        1
+    );
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (
+        " Grinch ",
+        10,
+        " M ",
+        " brown ",
+        " nice ",
+        1
+    );
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (
+        " Windy ",
+        10,
+        " M ",
+        " brown ",
+        " nice ",
+        1
+    );
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (
+        NULL,
+        10,
+        " M ",
+        " brown ",
+        " nice ",
+        1
+    );
